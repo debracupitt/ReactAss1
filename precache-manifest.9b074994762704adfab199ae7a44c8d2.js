25ea9b992c333d38358981a78b34c763bcf19b01
Debra Cupitt <debracupitt@gmail.com>
2019-02-11 11:52:47 +1100

    Updates

---
+++ precache-manifest.9b074994762704adfab199ae7a44c8d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f530678b0fcfd4f4b6b",
+    "url": "/debracupitt/ReactAss1.git/static/js/runtime~main.4f530678.js"
+  },
+  {
+    "revision": "da8c7a77536a27b3591d",
+    "url": "/debracupitt/ReactAss1.git/static/js/main.da8c7a77.chunk.js"
+  },
+  {
+    "revision": "0f0f1ed395de9ee32ca5",
+    "url": "/debracupitt/ReactAss1.git/static/js/2.0f0f1ed3.chunk.js"
+  },
+  {
+    "revision": "da8c7a77536a27b3591d",
+    "url": "/debracupitt/ReactAss1.git/static/css/main.5df60672.chunk.css"
+  },
+  {
+    "revision": "0afc1785f908885abe3bf44c06a74216",
+    "url": "/debracupitt/ReactAss1.git/index.html"
+  }
+];
